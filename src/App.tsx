9b16db07cc3b1117a8d50cf82f57c0c9ffd24ab9
Zen-333 <zenhamam56@gmail.com>
2026-01-07 15:11:42 +0000

    simple fix

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ const App: React.FC = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        <span className="heading">Aboozooz Task</span>
+        <span className="heading">Today's Tasks</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
         <TodoList 
         todos={todos} 
